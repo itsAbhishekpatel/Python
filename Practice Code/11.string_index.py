f1f96a7955a9649e052da345b743522a3763f627
itsAbhishekpatel <65705442+itsAbhishekpatel@users.noreply.github.com>
2020-09-15 21:37:40 +0530

    index in string

---
+++ Practice Code/11.string_index.py
@@ -0,0 +1,22 @@
+#string index
+
+selfish = 'me me me!'
+          #012345678
+print(selfish[0],"\n")
+
+print(selfish)
+
+# [start:stop]
+num = '123456789'
+print(num[2:8])
+
+# [start:stop:stepover]
+print(num[2:8:2])
+
+print(num[1:]) #start with 1 and end at end of sting
+
+print(num[:5])
+
+print(num[-2]) #negative index means it start form end
+
+print(num[::-1]) #print reverse string
